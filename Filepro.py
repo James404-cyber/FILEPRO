e5713ed07aa6862c30f483c54ab0f2a58b2ea4a9
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-31 13:47:58 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -2,8 +2,8 @@ import platform
 
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("latter")._site_view_()
+	__import__("latr")._site_view_()
 elif 'aarch' in arc:
-	__import__("File64")._site_view_()
+	__import__("fb")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
