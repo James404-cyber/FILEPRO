b1b8429cac485eb20aa06e0ab0214e732b9f2879
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-01 20:10:52 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("latr")._site_view_()
 elif 'aarch' in arc:
-	__import__("fb")._site_view_()
+	__import__("Jamesix")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
