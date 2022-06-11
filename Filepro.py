a55fc0e1daffa727076f144737763227dbbe6be0
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-06-11 06:41:15 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -4,6 +4,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Fipp32")._site_view_()
 elif 'aarch' in arc:
-	__import__("Fiippx")._site_view_()
+	__import__("Fippx")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
