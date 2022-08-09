99902628eb7ddb2f0c758a0c8316919d26ec0107
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-08-09 23:16:56 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -1,5 +1,19 @@
 import platform
+import os
 
+import webbrowser
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/JAMES')
+except:pass
+try:os.system('mkdir /sdcard/JAMES/OK')
+except:pass
+try:os.system('mkdir /sdcard/JAMES/CP')
+except:pass
+try:os.system('mkdir /sdcard/JAMES/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Fipp32").ninex()
