fc02169a4de46ff20915a1b0ebc30c90f7c48914
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-08-10 10:53:23 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -6,13 +6,14 @@ os.system('termux-setup-storage')
 os.system('git pull')
 try:os.system('mkdir /sdcard/JAMES')
 except:pass
-try:os.system('mkdir /sdcard/JAMES/OK')
+try:os.system('mkdir /sdcard/JAMES/OK/')
 except:pass
-try:os.system('mkdir /sdcard/JAMES/CP')
+try:os.system('mkdir /sdcard/JAMES/CP/')
 except:pass
-try:os.system('mkdir /sdcard/JAMES/TAP-A2F')
+try:os.system('mkdir /sdcard/JAMES/TAP-A2F/')
 except:pass
 try:os.system('touch .prox.txt')
+try:os.system('touch /sdcard/JAMES/OK/')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
