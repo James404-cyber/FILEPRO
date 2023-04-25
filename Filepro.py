a88017a458c1706fe54485ac8abe7478d16bdede
James404 <68128416+James404-cyber@users.noreply.github.com>
2023-04-25 08:57:07 +0300

    Update Filepro.py

--- Filepro.py
+++ Filepro.py
@@ -20,6 +20,6 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
 	__import__("Fipp32").ninex()
 elif 'aarch' in arc:
-	__import__("Pro").ninex()
+	__import__("Ulib8").ninex()
 else:
 	exit(f' Unknow device machine {arc}')
